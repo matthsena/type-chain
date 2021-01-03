fe1ba4e31f35adc0771d29b90ceccda9b9fb8491
Matheus <matheusalexandre.sena99@gmail.com>
2021-01-02 23:24:45 -0300

    Creating Genesis block

--- src/blockchain/index.ts
+++ src/blockchain/index.ts
@@ -1,7 +1,21 @@
-import * as block from '../block';
+import block from '../block';
 
 const blockchain = () => {
   const difficulty = 4;
   const pendingTransactions = [];
   const miningReward = 10;
+
+  const createGenesisBlock = () => {
+    const genesisBlock = block({
+      timestamp: Date.now(),
+      transactions: pendingTransactions,
+      previusHash: '0'
+    });
+
+    return genesisBlock.block;
+  };
+
+  const chain = [createGenesisBlock()];
 };
+
+export default block;

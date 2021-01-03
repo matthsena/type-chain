2a4356da35474d677dc5912232a8ec4d6854906c
Matheus <matheusalexandre.sena99@gmail.com>
2021-01-03 00:46:31 -0300

    sample running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello blockchain');
+import blockchain from './blockchain';
+import transactions from './transactions';
+
+const typeCoin = blockchain();
+
+const transaction1 = transactions('address1', 'address2', 100);
+typeCoin.createTransaction(transaction1);
+
+const transaction2 = transactions('address2', 'address1', 30);
+typeCoin.createTransaction(transaction2);
+
+console.log('Start mining');
+typeCoin.minePendingTransactions('address3');
+
+console.log('address1: ', typeCoin.getBalanceOfAddress('address1'));
+console.log('address2: ', typeCoin.getBalanceOfAddress('address2'));
+console.log('address3: ', typeCoin.getBalanceOfAddress('address3'));

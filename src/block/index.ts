fd817b34f6243aa691c5ee4f9ecd28b0d7ee2c4a
Matheus <matheusalexandre.sena99@gmail.com>
2021-01-02 22:57:09 -0300

    Function to miner new blocks

--- src/block/index.ts
+++ src/block/index.ts
@@ -9,13 +9,24 @@ interface IBlockParams<T> {
 const createBlock = <T>(params: IBlockParams<T>) => {
   const { timestamp, transactions, previusHash } = params;
 
-  const nonce = 0;
+  let nonce = 0;
 
   const calculateHash = (): string => SHA256(
     timestamp + previusHash + JSON.stringify(transactions) + nonce
   ).toString();
 
-  const hash = calculateHash();
+  let hash = calculateHash();
+
+  const mineNewBlock = (difficulty: number): void => {
+    while (hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+      nonce += 1;
+      hash = calculateHash();
+    }
+
+    console.log('👷‍♂️ A new block has been mined: ', hash);
+  };
+
+  return { calculateHash, mineNewBlock };
 };
 
 export default createBlock;

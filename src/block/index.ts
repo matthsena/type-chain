4fc7315e7f67958e030ef06d96e5e0f8a4af69ec
Matheus <matheusalexandre.sena99@gmail.com>
2021-01-02 22:48:28 -0300

    Create a function to calculate hash of block

--- src/block/index.ts
+++ src/block/index.ts
@@ -10,6 +10,12 @@ const createBlock = <T>(params: IBlockParams<T>) => {
   const { timestamp, transactions, previusHash } = params;
 
   const nonce = 0;
+
+  const calculateHash = (): string => SHA256(
+    timestamp + previusHash + JSON.stringify(transactions) + nonce
+  ).toString();
+
+  const hash = calculateHash();
 };
 
 export default createBlock;
